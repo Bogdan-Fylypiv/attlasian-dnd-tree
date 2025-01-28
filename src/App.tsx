6888e81d68426a23d137d2a8e9221eaf5503faec
Bogdan Fylypiv <bogdanfylypiv@Bogdans-MacBook-Pro-2.local>
2025-01-28 22:23:17 +0100

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,8 @@
-// src/App.tsx
-import React from 'react';
-import { TreeContext } from '../pragmatic-drag-and-drop/documentation/examples/pieces/tree/tree-context';
-import TreeItem from '../pragmatic-drag-and-drop/documentation/examples/pieces/tree/tree-item';
+import React from "react";
+import Example from "./example";
 
-const App: React.FC = () => {
-  return (
-    <TreeContext>
-      <h1>Drag-and-Drop Tree</h1>
-      <div>
-        {/* Render your tree items here */}
-        <TreeItem label="Item 1" />
-        <TreeItem label="Item 2">
-          <TreeItem label="Item 2.1" />
-        </TreeItem>
-        <TreeItem label="Item 3" />
-      </div>
-    </TreeContext>
-  );
+const App = () => {
+  return <Example />;
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "@atlaskit/css-reset";
+import AppProvider from "@atlaskit/app-provider";
+import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+<AppProvider children={<App />}>
+
+</AppProvider>,
+ document.getElementById("root")
 );
